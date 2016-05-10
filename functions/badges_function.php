7d9657bff6c80744c0ba21b59e8ab3758f24d71c
Tony <li.tony98@gmail.com>
2016-05-10 02:15:58 -0500

    Update badges_function.php

--- functions/badges_function.php
+++ functions/badges_function.php
@@ -2,14 +2,14 @@
 function badge_compiler($champion) {
 	//pogchamp
 	if ($champion >= 80000) {
-		return "<img src='imgs/pogchamp.jpg'>";
+		return "<img src='imgs/pogchamp.png'>";
 		}
 	//minglee
 	elseif ($champion > 3000 && $champion < 80000) {
-		return "<img src='imgs/minglee.jpg'>";
+		return "<img src='imgs/minglee.png'>";
 		}
 	//catface
 	elseif ($champion <= 3000) {
-		return "<img src='imgs/lirikN.jpg'>";
+		return "<img src='imgs/lirikN.png'>";
 		};
 };

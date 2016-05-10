b7cc088abf2ab12c89432625da500059e181b6da
Tony <li.tony98@gmail.com>
2016-05-10 02:09:53 -0500

    Update master_function.php

--- functions/master_function.php
+++ functions/master_function.php
@@ -10,11 +10,11 @@ $summoner_name = str_replace(" ", "", $_POST['summonername']);
 $summoner_name = strtolower($summoner_name);
 
 //convert summoner name into summoner ID
-	$parsed_summoner_id = get_api("https://na.api.pvp.net/api/lol/$region/v1.4/summoner/by-name/" . $summoner_name . "?api_key=" . $api_key_mike);
+	$parsed_summoner_id = get_api("https://na.api.pvp.net/api/lol/$region/v1.4/summoner/by-name/" . $summoner_name . "?api_key=" . $api_key_1);
 	$summoner_id = $parsed_summoner_id->$summoner_name->id;
 
 //get current match data and mastery data for each players in the match
-	$parsed_current_match = get_api("https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/" . $summoner_id . "?api_key=" . $api_key_mike);
+	$parsed_current_match = get_api("https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/" . $summoner_id . "?api_key=" . $api_key_1);
 	//store summoner name, summoner ID, and champ into storage $match_players
 	foreach ($parsed_current_match->participants as $data) {
 		$match_players[$data->summonerId] = array(

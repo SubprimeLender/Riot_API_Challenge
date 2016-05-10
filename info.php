a42e6f4776860d03b30f4c63ba8cb64f397d4559
Tony <li.tony98@gmail.com>
2016-05-10 02:45:13 -0500

    Update info.php

--- info.php
+++ info.php
@@ -1,47 +1,53 @@
 <html>
-  <head>
-  <title></title>
-  </head>
-  <body>
-    <div class="search-form-container">
-      <form action="mastery_result.php" method="POST" class="search-form">
-	    Summoner Name:
-      <input type="text" name="summonername" id="summonername">
-      <input type="submit" value="Submit">
-    </form>
-    </div>
-    <div class="info-container">
-      <div class="info-header"> What do all these badges mean? </div>
-      <div class="info-text">
-        <p>Badges are a visual representation of your rank in the world of champion mastery. Some badges represent the hard work you have put into a particular champion. While others may display your lack of experience. The list below explains what each badge means.</p>
-      <div class="badge">
-       <img src="/imgs/pogchamp.jpg" />
-       <p>PogChamp! You are the kid on the block who plays for keeps. No one's pogs are safe. If they don't know already, they are about to after this game. You don't let limits or rules stop your quest of mastery. In fact, you have more than doubled the required experience necessary for rank 5 mastery.  </p>
-      </div>
-      <div class="badge">
-       <img src="/imgs/minglee.jpg" />
-       <p>Just like twitch admins, we are unsure if you are here to help us, or ban us. You have played enough to get rank 5 mastery. But have yet to reach PogChap level. You can either be a God, or freelo. You control your own destiny and we pray you are the latter.</p>
-      </div>
-      <div class="badge">
-       <img src="/imgs/lirikN.jpg" />
-       <p>Wow.... Our cat is as scared of your mastery score as we are. You haven't mastered anything yet. If Mister Miyagi was still around, we would have him teach you the ways of APM. But keep playing and practicing. One day, you could be as good as imaqtpie.... Kappa. </p>
-      </div>
-      <div class="badge">
-       <img src="/imgs/biblethump.jpg" />
-       <p>Oh no. You aren't going to like what you are about to read. Your mastery score is higher than your entire team's combined. Essentially, you have a full team of potatoes. I hope your lifting belt from Amazon came in, because you are going to need it. </p>
-      </div>
-      <div class="badge">
-       <img src="/imgs/KappaOriginal.jpg" />
-       <p>Well well well. Looks like someone just went semi-pro. Your mastery alone is greater than the entire opponents combined. You are either level 100 super saiyan troll who only knows how to play Teemo. Or you are a bad ass who charges people Benjamins an hour to teach them your ways. Either way, I suspect a surrender at 20 for your opponent or you might as well call it a lost. </p>
-      </div>
-      <div class="badge">
-       <img src="/imgs/salt.jpg" />
-       <p>You might want to get the salt ready. Your team's combined mastery is lower than the other teams. You either have a bunch of kappas on your team or you yourself, are a kappa. Either way, you are likely to put that beer salt to a good use. </p>
-      </div>
-      <div class="badge">
-       <img src="/imgs/kappa.jpg" />
-       <p>Christmas just came early. And you just might be the lucky kid who has an N64 under the tree. Your team's mastery is greater than your opponents. But just like that fateful x-mas, the square shape box could be a dreamcast and not a playstation. So don't hold your breath. </p>
-      </div>
-    </div>
-  </body>
+<head>
+<title>Emotes Info</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  	<style>
+	  	.badge {
+		  	max-width: 500px;
+	  	}
+	</style>
+</head>
+<body>
+<div class="info-container">
+	<div class="info-header"> What do all these badges mean? </div>
+		<div class="info-text">
+		<p>Badges are a visual representation of your rank in the world of champion mastery. Some badges represent the hard work you have put into a particular champion. While others may display your lack of experience. The list below explains what each badge means.</p>
+		<div class="badge">
+		<img src="imgs/pogchamp.png" />
+		</div>
+		<p>PogChamp! You are the kid on the block who plays for keeps. No one's pogs are safe. If they don't know already, they are about to after this game. You don't let limits or rules stop your quest of mastery. In fact, you have more than doubled the required experience necessary for rank 5 mastery.  </p>
+	
+		<div class="badge">
+		<img src="imgs/minglee.png" />
+		</div>
+		<p>Just like twitch admins, we are unsure if you are here to help us, or ban us. You have played enough to get rank 5 mastery. But have yet to reach PogChap level. You can either be a God, or freelo. You control your own destiny and we pray you are the latter.</p>
+		
+		<div class="badge">
+		<img src="imgs/lirikN.png" />
+		</div>
+		<p>Wow.... Our cat is as scared of your mastery score as we are. You haven't mastered anything yet. If Mister Miyagi was still around, we would have him teach you the ways of APM. But keep playing and practicing. One day, you could be as good as imaqtpie.... Kappa. </p>
+
+		<div class="badge">
+		<img src="imgs/biblethump.png" />
+		</div>
+		<p>Oh no. You aren't going to like what you are about to read. Your mastery score is higher than your entire team's combined. Essentially, you have a full team of potatoes. I hope your lifting belt from Amazon came in, because you are going to need it. </p>
+
+		<div class="badge">
+		<img src="imgs/KappaOriginal.png" />
+		</div>
+		<p>Well well well. Looks like someone just went semi-pro. Your mastery alone is greater than the entire opponents combined. You are either level 100 super saiyan troll who only knows how to play Teemo. Or you are a bad ass who charges people Benjamins an hour to teach them your ways. Either way, I suspect a surrender at 20 for your opponent or you might as well call it a lost. </p>
+	
+		<div class="badge">
+		<img src="imgs/salt.png" />
+		</div>
+		<p>You might want to get the salt ready. Your team's combined mastery is lower than the other teams. You either have a bunch of kappas on your team or you yourself, are a kappa. Either way, you are likely to put that beer salt to a good use. </p>
+		
+		<div class="badge">
+		<img src="imgs/kappa.png" />
+		</div>
+		<p>Christmas just came early. And you just might be the lucky kid who has an N64 under the tree. Your team's mastery is greater than your opponents. But just like that fateful x-mas, the square shape box could be a dreamcast and not a playstation. So don't hold your breath. </p>
+		</div>
+    	</div>
+</body>
 </html>
